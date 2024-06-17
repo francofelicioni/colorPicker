282ef0bdf436c8318674c99747e36d1fc9dfdfe7
Fran Felicioni <ffelicioni@rica.design>
2024-06-17 19:15:56 +0200

    Move component directory

---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+
+
+const  Header = () => {
+  return (
+    <div className="flex justify-center items-center gap-x-8 py-4">
+        <img src="icons/paleta.svg" alt="paleta" className='Header-icon' />
+        <h1 className='text-2xl font-bold text-orange-200'>Color Palette Generator</h1>
+    </div>
+  );
+}
+
+export default Header;
