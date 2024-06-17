56731bde40bb9fa5f18928f71184eb3114da4d1d
Fran Felicioni <ffelicioni@rica.design>
2024-06-17 19:15:24 +0200

    Update App layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import { CompactPicker } from 'react-color';
 import './App.css';
-import Header from './components/header/header';
+import SelectorContainer from './components/SelectorContainer';
+import Header from './components/header';
+import Picker from './components/Picker';
+import { useState } from 'react';
 
 function App() {
 
+  const [colorSelected, setColorSelected] = useState('')
+
   return (
-    <div className="App">
+    <div className="flex flex-col justify-center items-center gap-y-8 bg-[#2C2C2C] h-screen w-screen p-4">
       <Header />
+      <SelectorContainer colorSelected={colorSelected}/>
+      <Picker colorSelected={colorSelected} setColorSelected={setColorSelected} />
     </div>
   );
 }
 
 export default App;
+
