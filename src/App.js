cac4898c0d3a868dd2409f353cb1d9bdeb6bac04
Fran Felicioni <ffelicioni@rica.design>
2024-06-23 20:23:09 +0200

    fix: update NameInput component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Header from './components/Header';
 import SelectorContainer from './components/SelectorContainer';
 import Picker from './components/Picker';
+import NameInput from './components/NameInput';
 
 function App() {
 
@@ -14,7 +15,10 @@ function App() {
     <div className="flex flex-col justify-center items-center gap-y-8 bg-[#2C2C2C] h-screen w-screen p-4">
       <Header />
       <SelectorContainer colorSelected={colorSelected} colors={colors} setColors={setColors} activeIndex={activeIndex} setActiveIndex={setActiveIndex}/>
-      <Picker colorSelected={colorSelected} setColorSelected={setColorSelected} colors={colors} setColors={setColors} index={activeIndex}/>
+      <div className='flex align-center justify-between gap-x-8'>
+        <Picker colorSelected={colorSelected} setColorSelected={setColorSelected} colors={colors} setColors={setColors} index={activeIndex}/>
+        <NameInput colors={colors} setColors={setColors} setActiveIndex={setActiveIndex}/>
+      </div>
     </div>
   );
 }
--- src/components/NameInput.jsx
+++ src/components/NameInput.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const NameInput = () => {
+const NameInput = ({colors, setColors, setActiveIndex}) => {
+
+  const [name, setName] = useState('')
+
+  const handleClick = () => {
+    if (name === '') {
+      console.log('Please enter a name')
+      return
+    }
+
+    if (colors.some(color => color === '')) {
+      console.log('Please pick a color for each palette')
+      return
+    }
+
+    localStorage.setItem('savedSchema', JSON.stringify(colors));
+    setColors(Array(5).fill(''))
+    setActiveIndex(null)
+    setName('')
+  }
+
   return (
-    <div>NameInput</div>
+    <div className='space-y-4'>
+      <p className='text-white font-bold'>Name:</p>
+      <div className='flex gap-x-2'>
+        <input type="text" className='bg-black text-white border border-white rounded px-2 py-1' placeholder='Add a schema name' value={name} onChange={(e) => setName(e.target.value)} />
+        <div className='cursor-pointer' onClick={handleClick}>
+          <svg width="67" height="42" viewBox="0 0 67 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect x="2" y="2" width="63" height="38" rx="7" fill="white" stroke="white" stroke-width="4" />
+            <line x1="35.6" y1="10" x2="35.6" y2="31" stroke="#2C2C2C" stroke-width="3" />
+            <line x1="46" y1="20.6" x2="25" y2="20.6" stroke="#2C2C2C" stroke-width="3" />
+          </svg>
+        </div>
+      </div>
+    </div>
   )
 }
 
